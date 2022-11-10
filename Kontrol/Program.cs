738faed754b2302193fd609bf7f2842402b1008e
elena tarasova <nafanichka.ev@gmail.com>
2022-11-10 13:18:42 -0500

    identify arrays

---
+++ Kontrol/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Clear();
+string[] PrimerArray = new string[] { "medicine", "14", "table", "hey", "34", "6534", "165", "sink", "hello", "ok", "low" };
+string[] NewArray = new string[PrimerArray.Length];
---
+++ Kontrolnaya/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+string[] PrimerArray = new string[] { "medicine", "14", "table", "hey", "34", "6534", "165", "sink", "hello", "ok", "low" };
+string[] NewArray = new string[PrimerArray.Length];
+
+void ArrayWithCondition(string[] PrimarArray, string[] NewArray)
+{
+    int j = 0;
+    for (int i = 0; i < PrimerArray.Length; i++)
+    {
+        Console.Write($"{PrimerArray[i]} ");
+        if (PrimerArray[i].Length <= 3)
+        {
+            NewArray[j] = PrimerArray[i];
+            j++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine($"Primer array: ");
+ArrayWithCondition(PrimerArray, NewArray);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("New array with condition:");
+PrintArray(NewArray);
